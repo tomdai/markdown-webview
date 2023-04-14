1f9ef65b51fc49efdb5c79c16a535230039c57e5
Tom <5054148+tomdai@users.noreply.github.com>
2023-04-14 13:09:51 -0700

    Combines `MarkdownWebView` definitions for macOS and iOS into one

--- Sources/MarkdownWebView/MarkdownWebView.swift
+++ Sources/MarkdownWebView/MarkdownWebView.swift
@@ -1,10 +1,14 @@
-#if os(macOS)
-
 import SwiftUI
 import WebKit
 
-@available(macOS 11.0, *)
-public struct MarkdownWebView: NSViewRepresentable {
+#if os(macOS)
+typealias PlatformViewRepresentable = NSViewRepresentable
+#elseif os(iOS)
+typealias PlatformViewRepresentable = UIViewRepresentable
+#endif
+
+@available(macOS 11.0, iOS 14.0, *)
+public struct MarkdownWebView: PlatformViewRepresentable {
     let markdownContent: String
     
     public init(_ markdownContent: String) {
@@ -13,30 +17,58 @@ public struct MarkdownWebView: NSViewRepresentable {
     
     public func makeCoordinator() -> Coordinator { .init(parent: self) }
     
-    public func makeNSView(context: Context) -> CustomWebView { context.coordinator.nsView }
+    #if os(macOS)
+    public func makeNSView(context: Context) -> CustomWebView { context.coordinator.platformView }
+    #elseif os(iOS)
+    public func makeUIView(context: Context) -> CustomWebView { context.coordinator.platformView }
+    #endif
     
-    public func updateNSView(_ nsView: CustomWebView, context: Context) {
-        guard !nsView.isLoading else { return } /// This function might be called when the page is still loading, at which time `window.proxy` is not available yet.
-        nsView.updateMarkdownContent(self.markdownContent)
+    func updatePlatformView(_ platformView: CustomWebView, context: Context) {
+        guard !platformView.isLoading else { return } /// This function might be called when the page is still loading, at which time `window.proxy` is not available yet.
+        platformView.updateMarkdownContent(self.markdownContent)
     }
+    
+    #if os(macOS)
+    public func updateNSView(_ nsView: CustomWebView, context: Context) { self.updatePlatformView(nsView, context: context) }
+    #elseif os(iOS)
+    public func updateUIView(_ uiView: CustomWebView, context: Context) { self.updatePlatformView(uiView, context: context) }
+    #endif
     
     public class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
         let parent: MarkdownWebView
-        let nsView: CustomWebView
+        let platformView: CustomWebView
         
         init(parent: MarkdownWebView) {
             self.parent = parent
             
             let webViewConfiguration: WKWebViewConfiguration = .init()
-            self.nsView = .init(frame: .zero, configuration: webViewConfiguration)
+            self.platformView = .init(frame: .zero, configuration: webViewConfiguration)
             
             super.init()
             
-            self.nsView.navigationDelegate = self
-            self.nsView.uiDelegate = self
-            self.nsView.setContentHuggingPriority(.required, for: .vertical)
+            self.platformView.navigationDelegate = self
+            self.platformView.uiDelegate = self
             
-            self.nsView.setValue(true, forKey: "drawsTransparentBackground")
+            #if DEBUG && os(iOS)
+            if #available(iOS 16.4, *) {
+                self.platformView.isInspectable = true
+            }
+            #endif
+            
+            /// So that the `View` adjusts its height automatically.
+            self.platformView.setContentHuggingPriority(.required, for: .vertical)
+            
+            /// Disables scrolling.
+            #if os(iOS)
+            self.platformView.scrollView.isScrollEnabled = false
+            #endif
+            
+            /// Set transparent background.
+            #if os(macOS)
+            self.platformView.setValue(true, forKey: "drawsTransparentBackground")
+            #elseif os(iOS)
+            self.platformView.isOpaque = false
+            #endif
             
             guard let templateFileURL = Bundle.module.url(forResource: "template", withExtension: ""),
                   let templateString = try? String(contentsOf: templateFileURL),
@@ -48,9 +80,10 @@ public struct MarkdownWebView: NSViewRepresentable {
             let htmlString = templateString
                 .replacingOccurrences(of: "PLACEHOLDER_SCRIPT", with: scriptString)
                 .replacingOccurrences(of: "PLACEHOLDER_STYLESHEET", with: stylesheetString)
-            self.nsView.loadHTMLString(htmlString, baseURL: nil)
+            self.platformView.loadHTMLString(htmlString, baseURL: nil)
         }
         
+        /// Update the content on first finishing loading.
         public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             (webView as! CustomWebView).updateMarkdownContent(self.parent.markdownContent)
         }
@@ -59,19 +92,24 @@ public struct MarkdownWebView: NSViewRepresentable {
     public class CustomWebView: WKWebView {
         var contentHeight: Double = 0
         
-        public override var intrinsicContentSize: NSSize {
-            let width = super.intrinsicContentSize.width
-            return .init(width: width, height: self.contentHeight)
+        public override var intrinsicContentSize: CGSize {
+            .init(width: super.intrinsicContentSize.width, height: self.contentHeight)
         }
         
+        /// Disables scrolling.
+        #if os(macOS)
         public override func scrollWheel(with event: NSEvent) {
             self.nextResponder?.scrollWheel(with: event)
             return
         }
+        #endif
         
+        /// Removes "Reload" from the context menu.
+        #if os(macOS)
         public override func willOpenMenu(_ menu: NSMenu, with event: NSEvent) {
             menu.items.removeAll { $0.identifier == .init("WKMenuItemIdentifierReload") }
         }
+        #endif
         
         func updateMarkdownContent(_ markdownContent: String) {
             guard let markdownContentBase64Encoded = markdownContent.data(using: .utf8)?.base64EncodedString() else { return }
@@ -86,5 +124,3 @@ public struct MarkdownWebView: NSViewRepresentable {
         }
     }
 }
-
-#endif
