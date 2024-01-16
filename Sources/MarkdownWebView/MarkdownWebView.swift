a76aca9ad13791a6ef51473e2468413e5f483472
Tuan Anh Do <kxkt06@gmail.com>
2024-01-16 01:23:46 +0100

    Fixed scrolling behaviour

--- Sources/MarkdownWebView/MarkdownWebView.swift
+++ Sources/MarkdownWebView/MarkdownWebView.swift
@@ -173,11 +173,7 @@ public struct MarkdownWebView: PlatformViewRepresentable {
         /// Disables scrolling.
         #if os(macOS)
         public override func scrollWheel(with event: NSEvent) {
-            if event.deltaY == 0 {
-                super.scrollWheel(with: event)
-            } else {
-                self.nextResponder?.scrollWheel(with: event)
-            }
+            self.nextResponder?.scrollWheel(with: event)
         }
         #endif
         
